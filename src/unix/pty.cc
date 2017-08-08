915707b969e0602a99dc18c0b50fd8f4ab872a2f
Adrian Pike <adrian@adrianpike.com>
2017-08-08 12:16:02 -0700

    temp. hack to disable echo

--- src/unix/pty.cc
+++ src/unix/pty.cc
@@ -199,7 +199,7 @@ NAN_METHOD(PtyFork) {
   }
   term->c_oflag = OPOST | ONLCR;
   term->c_cflag = CREAD | CS8 | HUPCL;
-  term->c_lflag = ICANON | ISIG | IEXTEN | ECHO | ECHOE | ECHOK | ECHOKE | ECHOCTL;
+  term->c_lflag = ICANON | ISIG | IEXTEN | ECHOE | ECHOK | ECHOKE | ECHOCTL;
 
   term->c_cc[VEOF] = 4;
   term->c_cc[VEOL] = -1;
